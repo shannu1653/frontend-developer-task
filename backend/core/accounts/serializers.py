5bbe79e026118d28f32996b2bfc8cdd1466b4409
shannu1653 <pentashanmukha2002@gmail.com>
2026-02-11 11:43:30 +0530

    changes

--- backend/core/accounts/serializers.py
+++ backend/core/accounts/serializers.py
@@ -13,6 +13,24 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         email = validated_data['email']
+
+        user = User.objects.create(
+            email=email,
+            username=email.split('@')[0]   # auto username
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+
+    class Meta:
+        model = User
+        fields = ('email', 'password')
+
+    def create(self, validated_data):
+        email = validated_data['email']
         username = email.split("@")[0]
 
         user = User.objects.create(
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://frontend-developer-task-omcr.onrender.com/api/",
 });
 
 api.interceptors.request.use((config) => {
