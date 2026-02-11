7bc516e5b1021b42fa5074bde211051bf7dac022
shannu1653 <pentashanmukha2002@gmail.com>
2026-02-11 12:19:58 +0530

    add meta

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://frontend-developer-task-omcr.onrender.com/api/",
+  baseURL:import.meta.env.VITE_API_URL,
+
 });
 
 api.interceptors.request.use((config) => {
